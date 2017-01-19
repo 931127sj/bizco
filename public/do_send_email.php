fd814c9d4bc3d1f61b6d7ddc9538a2521d36d2ac
dragonsuperf <dragonsuperf@gmail.com>
2017-01-19 10:22:09 +0900

    do_send_email , manage_join_user
    전체 메세지 보내기 픽스 , 참가자 확인 픽

--- public/do_send_email.php
+++ public/do_send_email.php
@@ -1,4 +1,89 @@
 <?php
+<<<<<<< HEAD
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    require '_common.php';
+
+    $qs = mysql_query("SELECT `user`.`email`
+                   FROM `user`
+                   WHERE `user`.`level` < 7
+                   ORDER BY `user`.`idx`");
+
+
+    date_default_timezone_set('Etc/UTC');
+    require '../librarys/PHPMailerAutoload.php';
+    //Create a new PHPMailer instance
+
+    $mail = new PHPMailer;
+    //Tell PHPMailer to use SMTP
+
+    $mail->isSMTP();
+
+    //Enable SMTP debugging
+    // 0 = off (for production use)
+    // 1 = client messages
+    // 2 = client and server messages
+    //$mail->SMTPDebug = 2;
+
+    //Ask for HTML-friendly debug output
+    $mail->Debugoutput = 'html';
+    //Set the hostname of the mail server
+    $mail->Host = 'smtp.gmail.com';
+    // use
+    // $mail->Host = gethostbyname('smtp.gmail.com');
+    // if your network does not support SMTP over IPv6
+    //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+    $mail->Port = 587;
+
+    //Set the encryption system to use - ssl (deprecated) or tls
+    $mail->SMTPSecure = 'tls';
+
+    //Whether to use SMTP authentication
+    $mail->SMTPAuth = true;
+
+    //Username to use for SMTP authentication - use full email address for gmail
+    $mail->Username = "sbecenterformail@gmail.com";
+
+    //Password to use for SMTP authentication
+    $mail->Password = "frog0704";
+
+    //Set who the message is to be sent from
+    $mail->setFrom('from@example.com', 'SBE CENTER');
+
+    //Set an alternative reply-to address
+    $mail->addReplyTo('replyto@example.com', 'SBE CENTER');
+
+    //Set who the message is to be sent to
+    while($row = mysql_fetch_array($qs))
+    {
+        $mail->addAddress($row['email']);
+    }
+
+    //Set the subject line
+    $mail->Subject = $title;
+    //Read an HTML message body from an external file, convert referenced images to embedded,
+    //convert HTML into a basic plain-text alternative body
+
+    $mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+
+    //Replace the plain text body with one created manually
+    $mail->Body = $content;
+
+    //Attach an image file
+    //$mail->addAttachment('images/phpmailer_mini.png');
+
+    //send the message, check for errors
+
+    if (!$mail->send()) {
+        msg("메일을 보내는데 실패하였습니다." .$mail->ErrorInfo);
+        back();
+        exit();
+    } else {
+        msg("메일을 성공적으로 보냈습니다.");
+        back();
+        exit();
+    }
+=======
 require '_common.php';
 error_reporting(-1);
 ini_set('display_errors', 'On');
@@ -25,3 +110,4 @@ if ($result) {
 }
 
 // return sendMail('test', ['sh.kang@ecubelabs.com'], 'sadasd', 'dasdasdasdasdas');
+>>>>>>> origin/master
--- views/pages/manage_join_user.php
+++ views/pages/manage_join_user.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <?php
 $q = $_GET['q'];
 $company_id = $_SESSION['company'];
@@ -8,140 +7,33 @@ if ($q)
 else
     $search_query = "";
 
-// print_column 가공
-$qs = mysql_query("SELECT `cs`.`idx` as `step_idx`,
-                          `cs`.`step_seq`,
-                          `article`.`idx` as `article_idx`
-                   FROM `curriculum_step` as `cs`
-                   LEFT JOIN `article`
-                   ON `article`.`step_id` = `cs`.`step_seq`
-                   ORDER BY `cs`.`step_seq`, `article`.`idx` ASC");
-$print_column = array();
-$step_count = 0;
-while ($row = mysql_fetch_array($qs)) {
-    $print_column[$row['step_seq']][] = $row['article_idx'];
-    $step_count++;
-}
 
 $qs = mysql_query("SELECT `user`.`idx` as `user_idx`,
                           `user`.`name`,
-                          `article`.`idx` as `article_idx`,
-                          `article`.`title`, ifnull(`homework`.`state`, 0) as is_complete
+                          `user_step_relation`.`current_step_idx` ,
+                          ifnull(`homework`.`state`, 0) as is_complete
                    FROM `user`
+                   LEFT JOIN `user_step_relation`
+                   ON `user_step_relation`.`user_idx` = `user`.`idx`
                    LEFT JOIN `article`
-                   ON `article`.`board_id` = '{$company_id}_cur'
+                   ON `article`.`board_id` = 'dankook_cur'
+                   AND `article`.`step_id` = `user_step_relation`.`current_step_idx`
                    LEFT JOIN `homework`
                    ON `homework`.`article_idx` = `article`.`idx`
                    AND `homework`.`user_idx` = `user`.`idx`
-                   LEFT JOIN `curriculum_step` as `cs`
-                   ON `cs`.`step_seq` = `article`.`step_id`
+                   LEFT JOIN `curriculum_step` as `cs`  
+                   ON `cs`.`idx` = `article`.`step_id`
                    WHERE `user`.`company_id` = '$company_id'
                    $search_query
-                   GROUP BY `user`.`idx`, `article`.`idx`
-                   ORDER BY `user`.`idx`, `cs`.`step_seq`, `article`.`idx` ASC");
+                   GROUP BY `user`.`idx`, `article`.`idx` , `user_step_relation`.`user_idx`
+                   ORDER BY `user`.`idx`, `cs`.`step_seq`, `article`.`idx` ASC, `user_step_relation`.`user_idx`");
 $print_users  = array();
 $print_users_name  = array();
 $print_users_title  = array();
 while ($row = mysql_fetch_array($qs)) {
     $print_users[$row['user_idx']][] = $row['is_complete'];
     $print_users_name[$row['user_idx']] = $row['name'];
-    $print_users_title[] = $row['title'];
-}
-
-$current_step = 'manage_join_user';
-?>
-<h2 class="ui header">참가자 과제확인</h2>
-<form class="ui clearing segment selene-basic">
-    <div class="ui icon input">
-        <input type="text" name="q" placeholder="이름 / 이메일로 찾기" value="<?=$q?>">
-        <i class="search link icon"></i>
-    </div>
-    <button class="ui button"><?= $lang_search ?></button>
-</form>
-
-<div style="overflow: auto; width: 100%;">
-  <table class="ui celled compact striped table">
-      <thead>
-          <tr>
-              <th>스텝</th>
-              <? foreach ($print_column as $key => $value) { ?>
-              <th colspan="<?=count($value)?>"><?=$key?></th>
-              <? } ?>
-          </tr>
-          <tr>
-              <th>과제</th>
-              <?  $i = 0;
-              foreach ($print_column as $key => $value) { ?>
-                <? foreach ($value as $key2 => $item) {
-                if ($step_count-1 == $i)
-                  break; ?>
-              <th data-content="<?=xssHtmlProtect($print_users_title[$i])?>" data-variation="mini" data-variation="inverted"><i class="check circle icon"></i></th>
-                <? $i++; } ?>
-              <? } ?>
-          </tr>
-      </thead>
-      <tbody>
-          <? foreach ($print_users as $key => $value) { ?>
-          <tr>
-              <td><?=$print_users_name[$key]?></td>
-              <? foreach ($value as $key2 => $item) { ?>
-              <td>
-                <?=$item?'<span style="color: green; font-weight: bold;">O</span>':'<span style="color: red; font-weight: bold;">X</span>'?>
-              </td>
-              <? } ?>
-          </tr>
-          <? } ?>
-      </tbody>
-  </table>
-</div>
-=======
-<?php
-$q = $_GET['q'];
-$company_id = $_SESSION['company'];
-
-if ($q)
-    $search_query = "AND (`user`.`name` like '%".mysql_escape_string($q)."%' OR `user`.`email` like '%".mysql_escape_string($q)."%')";
-else
-    $search_query = "";
-
-// print_column 가공
-$qs = mysql_query("SELECT `cs`.`idx` as `step_idx`,
-                          `cs`.`step_seq`,
-                          `article`.`idx` as `article_idx`
-                   FROM `curriculum_step` as `cs`
-                   LEFT JOIN `article`
-                   ON `article`.`step_id` = `cs`.`step_seq`
-                   ORDER BY `cs`.`step_seq`, `article`.`idx` ASC");
-$print_column = array();
-$step_count = 0;
-while ($row = mysql_fetch_array($qs)) {
-    $print_column[$row['step_seq']][] = $row['article_idx'];
-    $step_count++;
-}
-
-$qs = mysql_query("SELECT `user`.`idx` as `user_idx`,
-                          `user`.`name`,
-                          `article`.`idx` as `article_idx`,
-                          `article`.`title`, ifnull(`homework`.`state`, 0) as is_complete
-                   FROM `user`
-                   LEFT JOIN `article`
-                   ON `article`.`board_id` = '{$company_id}_cur'
-                   LEFT JOIN `homework`
-                   ON `homework`.`article_idx` = `article`.`idx`
-                   AND `homework`.`user_idx` = `user`.`idx`
-                   LEFT JOIN `curriculum_step` as `cs`
-                   ON `cs`.`step_seq` = `article`.`step_id`
-                   WHERE `user`.`company_id` = '$company_id'
-                   $search_query
-                   GROUP BY `user`.`idx`, `article`.`idx`
-                   ORDER BY `user`.`idx`, `cs`.`step_seq`, `article`.`idx` ASC");
-$print_users  = array();
-$print_users_name  = array();
-$print_users_title  = array();
-while ($row = mysql_fetch_array($qs)) {
-    $print_users[$row['user_idx']][] = $row['is_complete'];
-    $print_users_name[$row['user_idx']] = $row['name'];
-    $print_users_title[] = $row['title'];
+    $print_users_title[$row['user_idx']] = $row['current_step_idx'];
 }
 
 $current_step = 'manage_join_user';
@@ -159,30 +51,30 @@ $current_step = 'manage_join_user';
   <table class="ui celled compact striped table">
       <thead>
           <tr>
-              <th>스텝</th>
+              <th>이름</th>
+              <th>현재 스텝</th>
+              <th colspan = "13">진행 상황</th>
+              <!--
               <? foreach ($print_column as $key => $value) { ?>
               <th colspan="<?=count($value)?>"><?=$key?></th>
               <? } ?>
-          </tr>
-          <tr>
-              <th>과제</th>
-              <?  $i = 0;
-              foreach ($print_column as $key => $value) { ?>
-                <? foreach ($value as $key2 => $item) {
-                if ($step_count-1 == $i)
-                  break; ?>
-              <th data-content="<?=xssHtmlProtect($print_users_title[$i])?>" data-variation="mini" data-variation="inverted"><i class="check circle icon"></i></th>
-                <? $i++; } ?>
-              <? } ?>
+                -->
           </tr>
       </thead>
+      
       <tbody>
           <? foreach ($print_users as $key => $value) { ?>
           <tr>
-              <td><?=$print_users_name[$key]?></td>
+              <td>
+                  <?=$print_users_name[$key]?>
+              </td>
+              <td>
+                  <?=$print_users_title[$key]?>
+              </td>
               <? foreach ($value as $key2 => $item) { ?>
               <td>
                 <?=$item?'<span style="color: green; font-weight: bold;">O</span>':'<span style="color: red; font-weight: bold;">X</span>'?>
+                  
               </td>
               <? } ?>
           </tr>
@@ -190,4 +82,3 @@ $current_step = 'manage_join_user';
       </tbody>
   </table>
 </div>
->>>>>>> Stashed changes
